b59da42382b3bc249be9b98c5e9f9797a90a6346
tukkedd <jeamki2501@gmail.com>
2024-10-04 18:22:28 -0400

    endpoints

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ import { PORT } from './config.js'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.send('<h1>hello world</h1>')
 })
+
+app.post('/login', (req, res) => {})
+app.post('/register', (req, res) => {})
+app.post('/logout', (req, res) => {})
+
+app.post('/protected', (req, res) => {})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
