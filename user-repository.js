3ff1e0f2f32e6220df5b71904b555d6042883bd4
tukkedd <jeamki2501@gmail.com>
2024-10-05 19:05:40 -0400

    local db and create an user

---
+++ user-repository.js
@@ -0,0 +1,35 @@
+import dbLocal from "db-local";
+import crypto from 'crypto'
+
+const { Schema } = new dbLocal({ path: './db' })
+
+const User = Schema('User', {
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+})
+
+export class UserRepository {
+    static create({ username, password }) {
+        // 1 Validaciones de username (opcional: se puede hacer con zod)
+        if (typeof username !== 'string') throw new Error('username must be a string')
+        if (username.length < 3) throw new Error('username must be at least 2 characters long ')
+
+        if (typeof password !== 'string') throw new Error('passsword must be a string')
+        if (password.length < 6) throw new Error('password must be at least 6 characters long')
+
+        const user = User.findOne({ username })
+        if (user) throw new Error('username already exists')
+
+        const id = crypto.randomUUID()
+
+        User.create({
+            _id: id,
+            username,
+            password
+        }).save()
+
+        return id
+    }
+    static login({ username, password }) { }
+}
